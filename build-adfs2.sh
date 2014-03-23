289cdf54e4f6cc267f166ceefc43f13cadb5802c
Stefan Scherer <stefan@Stefans-MacBook-Pro.local>
2014-03-23 18:20:11 +0100

    removed comment

--- build-adfs2.sh
+++ build-adfs2.sh
@@ -1,6 +1,6 @@
 echo "starting adfs2 for first time without provision to set hostname"
 vagrant destroy adfs2 -f
-vagrant up adfs2 --provision # --no-provision
+vagrant up adfs2 --provision
 echo "rebooting adfs2 for provisioning, join windomain"
 vagrant reload adfs2 --provision
 echo "rebooting adfs2 to finalize provisioning"
--- build-win7.sh
+++ build-win7.sh
@@ -1,6 +1,6 @@
 echo "starting win7 for first time without provision to set hostname"
 vagrant destroy win7 -f
-vagrant up win7 --provision # --no-provision
+vagrant up win7 --provision
 echo "rebooting win7 for provisioning, join windomain"
 vagrant reload win7 --provision
 echo "rebooting win7 to finalize provisioning"
