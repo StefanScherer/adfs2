d9beff9c1a534fb2d9ce7ab596dec3dace0d8731
Stefan Scherer <stefan@Stefans-MacBook-Pro.local>
2014-03-27 22:51:32 +0100

    scheduled task also for battery mode

--- scripts/install-nd451.ps1
+++ scripts/install-nd451.ps1
@@ -23,6 +23,63 @@ If (-not (Test-Path $SealSetupInstaller)) {
   $SealSetupInstaller = "c:\vagrant\resources\fm\frozen\" + $Setup
 }
 
+
+# create a Task Scheduler task which is also able to run in battery mode due
+# to host notebooks working in battery mode. This complicates the whole script
+# from a one liner to a fat XML - good heaven.
+
+$xml = @"
+<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+    <Date>2014-03-27T13:53:05</Date>
+    <Author>vagrant</Author>
+  </RegistrationInfo>
+  <Triggers>
+    <TimeTrigger>
+      <StartBoundary>2014-03-27T00:00:00</StartBoundary>
+      <Enabled>true</Enabled>
+    </TimeTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <UserId>vagrant</UserId>
+      <LogonType>InteractiveToken</LogonType>
+      <RunLevel>LeastPrivilege</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>false</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
+    <IdleSettings>
+      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <RestartOnIdle>false</RestartOnIdle>
+    </IdleSettings>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+    <RunOnlyIfIdle>false</RunOnlyIfIdle>
+    <WakeToRun>false</WakeToRun>
+    <ExecutionTimeLimit>P3D</ExecutionTimeLimit>
+    <Priority>7</Priority>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>C:\Chocolatey\bin\AutoIt3.bat</Command>
+      <Arguments>C:\vagrant\scripts\install-nd451.au3 $SealSetupInstaller</Arguments>
+    </Exec>
+  </Actions>
+</Task>
+"@
+
+$XmlFile = $env:Temp + "\SealSetup.xml"
+Write-Host "Write Task to $XmlFile"
+$xml | Out-File $XmlFile
+
 Write-Host "Check $SealSetupInstaller"
 If (Test-Path $SealSetupInstaller) {
   If (-not (Test-Path c:\vagrant\resources\license.ini)) {
@@ -30,7 +87,8 @@ If (Test-Path $SealSetupInstaller) {
   }
 
   & schtasks /Delete /F /TN SealSetup
-  & schtasks /Create /SC ONCE /TN SealSetup /TR "C:\Chocolatey\bin\AutoIt3.bat C:\vagrant\scripts\install-nd451.au3 $SealSetupInstaller" /ST 00:00
+#  & schtasks /Create /TN SealSetup /RU vagrant /RP vagrant /XML $XmlFile
+  & schtasks /Create /TN SealSetup /XML $XmlFile
   & schtasks /Run /TN SealSetup
 }
 
