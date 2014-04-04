0b8e88a11638f3cc9d3ee064722aa701c77812ea
Stefan Scherer <scherer_stefan@icloud.com>
2014-04-04 12:59:00 +0200

    download fiddler4 on win8

--- scripts/install-fiddler.ps1
+++ scripts/install-fiddler.ps1
@@ -1,8 +1,19 @@
-if (!(Test-Path 'c:\Program Files (x86)\Fiddler2')) {
-  $ExeFile = $env:Temp + '\fiddler2setup.exe'
-  Write-Host "Downloading Fiddler2"
-  (New-Object Net.WebClient).DownloadFile('http://www.telerik.com/docs/default-source/fiddler/fiddler2setup.exe?sfvrsn=2', $ExeFile)
-  Write-Host "Installing Fiddler2"
-  & $ExeFile /S
+if ( [System.Environment]::OSVersion.Version.Build -lt 9600 ) {
+
+  if (!(Test-Path 'c:\Program Files (x86)\Fiddler2')) {
+    $ExeFile = $env:Temp + '\fiddler2setup.exe'
+    Write-Host "Downloading Fiddler2"
+    (New-Object Net.WebClient).DownloadFile('http://www.telerik.com/docs/default-source/fiddler/fiddler2setup.exe?sfvrsn=2', $ExeFile)
+    Write-Host "Installing Fiddler2"
+    & $ExeFile /S
+  }
+} else {
+  if (!(Test-Path 'c:\Program Files (x86)\Fiddler4')) {
+    $ExeFile = $env:Temp + '\fiddler4setup.exe'
+    Write-Host "Downloading Fiddler4"
+    (New-Object Net.WebClient).DownloadFile('http://www.telerik.com/docs/default-source/fiddler/fiddler4setup.exe?sfvrsn=2', $ExeFile)
+    Write-Host "Installing Fiddler4"
+    & $ExeFile /S
+  }
 }
 
