cc6875f397f09fefa4866ebc31594a48fed2eb8d
danielmenezesbr <danielmenezes@gmail.com>
2018-04-14 16:37:44 -0300

    Use atom 1.25.1

--- scripts/install-atom.ps1
+++ scripts/install-atom.ps1
@@ -1,6 +1,6 @@
-choco install -y atom --version 0.174.0
+choco install -y atom --version=1.25.1
 Write-Host $env:LOCALAPPDATA
-$env:PATH="$env:PATH;$env:LOCALAPPDATA\atom\bin"
+$env:PATH="$env:PATH;$env:LOCALAPPDATA\atom\app-1.25.1\resources\app\apm\bin"
 apm install language-powershell
 apm install language-batch
 apm install language-docker
